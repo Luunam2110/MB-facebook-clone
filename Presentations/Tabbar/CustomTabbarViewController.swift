67c6cd780414cbf86fa4c172c37883521f5cc642
namlvsaokhue <namlv@saokhue.io>
2023-08-24 23:42:49 +0700

    feat: selectedTabbar

--- Presentations/Tabbar/CustomTabbarViewController.swift
+++ Presentations/Tabbar/CustomTabbarViewController.swift
@@ -13,16 +13,26 @@ class CustomTabbarViewController: UIViewController, StoryboardInstantiable {
     var friendTab : UINavigationController?
     var notiTab : UINavigationController?
     var profileTab : UINavigationController?
-
+    
+    
+    var currentTab = 0;
+    
+    var leadingConstraint: NSLayoutConstraint?
+    var trailingConstraint: NSLayoutConstraint?
+    @IBOutlet weak var selectedIndicator: UIView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var tabView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        openHome()
+        openHome();
+        initView();
+        setSelectedTab(0);
     }
     @IBAction func onClickTabbar(_ sender: UIButton) {
         let tag = sender.tag
+        print(tag)
+        currentTab = tag;
+        setSelectedTab(tag);
         switch tag {
         case 1:
             openFriend()
@@ -32,6 +42,39 @@ class CustomTabbarViewController: UIViewController, StoryboardInstantiable {
             openProfile()
         default:
            openHome()
+        }
+        
+    }
+    func initView () {
+        let width = (Float(selectedIndicator.frame.width));
+        selectedIndicator.widthAnchor.constraint(equalToConstant: CGFloat(width )).isActive = true
+        leadingConstraint = selectedIndicator.leadingAnchor.constraint(equalTo: tabView.leadingAnchor)
+        leadingConstraint?.isActive = true
+        let maskPath = UIBezierPath(
+            roundedRect: selectedIndicator.bounds,
+            byRoundingCorners: [.bottomLeft, .bottomRight],
+            cornerRadii: CGSize(width: CGFloat(2.5), height: CGFloat(2.5))
+        )
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = maskPath.cgPath
+        selectedIndicator.layer.mask = maskLayer
+        selectedIndicator.layoutIfNeeded()
+    }
+
+    func setSelectedTab (_ tab : Int) {
+        let width = (Float(tabView.frame.width) / 4)
+        let indicatorWidth = (Float(selectedIndicator.frame.width))
+        UIView.animate(withDuration: 0.2, delay: 0 , options: .curveLinear) {
+            self.selectedIndicator.transform = CGAffineTransform(translationX: CGFloat(width * Float(tab) + (width - indicatorWidth) / 2 ), y: self.selectedIndicator.frame.minY)
+            if let  imageView = self.view.viewWithTag(tab) as? UIImageView {
+                imageView.tintColor = UIColor(named: "tabbarSelectedColor")
+            }
+            for i in 1...4 {
+                if let imageView = self.view.viewWithTag(i) as? UIImageView {
+                    imageView.tintColor = UIColor(named: i == tab + 1  ? "tabbarSelectedColor" : "tabbarColor")
+                }
+                
+            }
         }
         
     }
